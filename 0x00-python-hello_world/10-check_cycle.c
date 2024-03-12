cbf84abc13d14fe3dec7652ae2566eacb0dc9c75
LichtCode <prinzdarey@gmail.com>
2024-03-12 05:59:50 +0100

    adde things

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,26 @@
 #include "lists.h"
 
 /**
-* check_cycle - checks if a linked list contains a cycle
-* @list: linked list to check
-*
-* Return: 1 if the list has a cycle, 0 if it doesn't
-*/
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-		listint_t *w = list;
-		listint_t *t = list;
+	listint_t *w = list;
+	listint_t *t = list;
 
-		if (!list)
-			return (0);
-
-		while (w && t && t->l_next)
-		{
-			w = w->l_next;
-			t = t->l_next->l_next;
-			if (w == t)
-				return (1);
-		}
-
+	if (!list)
 		return (0);
+
+	while (w && t && t->l_next)
+	{
+		w = w->l_next;
+		t = t->l_next->l_next;
+		if (w == t)
+			return (1);
+	}
+
+	return (0);
 }
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,1 +1,1 @@
-# 0x00. Python - Hello, World tasks Task
+Python - Hello, World
