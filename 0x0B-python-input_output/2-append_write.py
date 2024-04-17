ba2aba025dfc3f7523e9e06aafddb53106810952
LichtCode <prinzdarey@gmail.com>
2024-04-17 04:05:45 +0100

    0x0B. Python - Input/Output Tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """a function that appends a string at the end of a 
+        text file (UTF8) and returns the number of characters.
+    Args:
+        filename (str): name of the file to append to.
+        text (str): string to append to the file.
+    Returns:
+        number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
