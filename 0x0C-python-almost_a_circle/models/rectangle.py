28c46f672663e0645823b9aa116cf3220c65312c
LichtCode <prinzdarey@gmail.com>
2024-04-27 05:53:01 +0100

    made some changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """
 Rectangle module
 """
@@ -34,19 +34,37 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def __str__(self):
+        """
+        Returns the print() and str() representation of the Rectangle.
+        """
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".format(self.id,
+                                                                self.__x,
+                                                                self.__y,
+                                                                self.__width,
+                                                                self.__height))
+
     @property
     def width(self):
-        """
-        get width
+        """get width.
+
+        Returns:
+            int: width of rectangle.
         """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """set Width.
+
+        Args:
+            value (int): width of rectangle.
+
+        Raises:
+            TypeError: if width is not an integer.
+            ValueError: if width is less than or equal to zero.
         """
-        set Width
-        """
-        if not isinstance(value, int) or isinstance(value, bool):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -54,37 +72,50 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """
-        get height
+        """get height.
+
+        Returns:
+            int: height of rectangle.
         """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        set height
+        """set height.
+
+        Args:
+            value (int): height of rectangle.
+
+        Raises:
+            TypeError: if height is not an integer.
+            ValueError: if height is less than or equal to zero.
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-
         self.__height = value
 
     @property
     def x(self):
-        """
-        get x
+        """get x.
+
+        Returns:
+            int: x.
         """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """set x.
+
+        Args:
+            value (int): x.
+
+        Raises:
+            TypeError: if x is not an integer.
+            ValueError: if x is less than or equal to zero.
         """
-        set x
-        """
-        # added a check for when value is a bool, if the check is removed
-        # the unittest for it will fail
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -93,15 +124,23 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """
-        get y
+        """get y.
+
+        Returns:
+            int: y.
         """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        set y
+        """set y.
+
+        Args:
+            value (int): y.
+
+        Raises:
+            TypeError: if y is not an integer.
+            ValueError: if y is less than or equal to zero.
         """
         if type(value) is not int:
             raise TypeError("y must be an integer")
@@ -110,17 +149,15 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """
-        Return the area of the rectangle
-        """
-        area = self.width * self.height
+        """Return the area of the rectangle.
 
-        return area
+        Returns:
+            int: area.
+        """
+        return self.__width * self.__height
 
     def display(self):
-        """
-        Print size of rectangle using #
-        """
+        """Print size of rectangle using #."""
         if self.__y > 0:
             for i in range(self.__y):
                 print()
@@ -132,21 +169,15 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
-    def __str__(self):
-        """
-        Returns the print() and str() representation of the Rectangle.
-        """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x,
-                                                       self.y,
-                                                       self.width,
-                                                       self.height)
-
     def update(self, *args, **kwargs):
+        """Assigns arguments to attributes based on their positions
+
+        Args:
+            *args (tuple): arguments.
+            **kwargs (dict): double pointer to a dictionary.
         """
-        Assigns arguments to attributes based on their positions.
-        """
-        if args is not None and len(args) != 0:
+
+        if args is not None and len(args) is not 0:
             list_atrr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_atrr[i], args[i])
@@ -155,18 +186,19 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-        Represent a dictionary representation of rectangle
-        """
-        rec_dict = {
-                "id": self.id,
-                "width": self.width,
-                "height": self.height,
-                "x": self.x,
-                "y": self.y,
-        }
+        """Represent a dictionary representation of rectangle.
 
-        return rec_dict
+        Returns:
+            dict: rectangle.
+        """
+        dict = {}
+        dict["id"] = self.id
+        dict["width"] = self.width
+        dict["height"] = self.height
+        dict["x"] = self.x
+        dict["y"] = self.y
+        return (dict)
+
 
 
 if __name__ == "__main__":
