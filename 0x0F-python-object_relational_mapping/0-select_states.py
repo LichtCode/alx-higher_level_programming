aa6e75bae65968196f3097d107727c40c01baef9
LichtCode <prinzdarey@gmail.com>
2024-06-28 21:09:30 +0100

    Adding more files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""list all states from the database"""
+"""
+Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
+states.id
+"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
 
-    import MySQLdb
-    import sys
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
-    db.close()
+    conn.close()
