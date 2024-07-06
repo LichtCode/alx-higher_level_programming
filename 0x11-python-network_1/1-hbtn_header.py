07d57f591de430888c3585e2db07a9e1ebd2f2f1
LichtCode <prinzdarey@gmail.com>
2024-07-06 04:10:40 +0100

    made more changes to the files

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,6 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
 import urllib.request
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,8 +2,7 @@
 """A script that:
 - takes in a URL
 - sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+- takes email as a parameter and displays the body
 """
 import sys
 import urllib.parse
