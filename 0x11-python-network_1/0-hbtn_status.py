780000c6ea7ea20a95931eb8c088233532a7ea19
LichtCode <prinzdarey@gmail.com>
2024-07-06 04:15:21 +0100

    made more changes to the files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
-- using the urlib package
 """
+from urllib import request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
