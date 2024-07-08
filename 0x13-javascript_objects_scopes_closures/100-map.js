bbe0e53a8e3d252ed90034282887f30c34a5737b
LichtCode <prinzdarey@gmail.com>
2024-07-08 03:10:21 +0100

    made changes to the files

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
+
+const mapList = list.map((element, idx) => element * idx);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(mapList);
